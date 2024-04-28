5de6ef0fc0c54416726a37ef5b9e5f6b7886e8d9
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-28 17:09:34 +0530

    add css styling - dynamic text and color

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const wakeLockBtn = document.getElementById("wakeLockBtn");
 const statusText = document.getElementById("status");
+const container = document.getElementById("container");
 
 let wakeLock = null;
 
-wakeLockBtn.addEventListener("click", async () => {
-	if (!wakeLock) {
-		try {
-			wakeLock = await navigator.wakeLock.request("screen");
-			statusText.textContent = "Wake Lock Acquired";
-		} catch (err) {
-			if (err.name === "NotAllowedError") {
-				statusText.textContent = "Wake Lock Permission Denied";
-			} else {
-				statusText.textContent = `Error: ${err}`;
-			}
+async function requestWakeLock() {
+	try {
+		wakeLock = await navigator.wakeLock.request("screen");
+		statusText.textContent = "Wake Lock Acquired";
+		container.className = "night-mode";
+		wakeLockBtn.textContent = "Release Wake Lock";
+	} catch (err) {
+		if (err.name === "NotAllowedError") {
+			statusText.textContent = "Wake Lock Permission Denied";
+		} else {
+			statusText.textContent = `Error: ${err}`;
 		}
-	} else {
+	}
+}
+
+wakeLockBtn.addEventListener("click", async () => {
+	if (wakeLock) {
 		wakeLock.release();
 		wakeLock = null;
+		container.className = "day-mode";
 		statusText.textContent = "Wake Lock Released";
+		wakeLockBtn.textContent = "Aquire Wake Lock";
+	} else {
+		requestWakeLock();
 	}
 });
+
+requestWakeLock();
