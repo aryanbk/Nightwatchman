dc7275713b9030fe283a044cfd8865b69fc53ea3
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-04-28 18:28:42 +0530

    add dynamic title

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const wakeLockBtn = document.getElementById("wakeLockBtn");
 const statusText = document.getElementById("status");
+const titleText = document.getElementById("title");
 const container = document.getElementById("container");
 
 let wakeLock = null;
@@ -7,9 +8,10 @@ let wakeLock = null;
 async function requestWakeLock() {
 	try {
 		wakeLock = await navigator.wakeLock.request("screen");
-		statusText.textContent = "Wake Lock Acquired";
 		container.className = "night-mode";
 		wakeLockBtn.textContent = "Release Wake Lock";
+		statusText.textContent = "NightWatchMan is awake..!";
+		titleText.textContent = "NightWatchMan is awake..!";
 	} catch (err) {
 		if (err.name === "NotAllowedError") {
 			statusText.textContent = "Wake Lock Permission Denied";
@@ -24,8 +26,9 @@ wakeLockBtn.addEventListener("click", async () => {
 		wakeLock.release();
 		wakeLock = null;
 		container.className = "day-mode";
-		statusText.textContent = "Wake Lock Released";
 		wakeLockBtn.textContent = "Aquire Wake Lock";
+		statusText.textContent = "NightWatchMan caught sleeping ᶻ 𝗓 𐰁";
+		titleText.textContent = "NightWatchMan caught sleeping ᶻ 𝗓 ";
 	} else {
 		requestWakeLock();
 	}
